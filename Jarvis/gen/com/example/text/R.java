432d01205e34997d9a4598a9a60d14d6e6c62487
raelix <raelix@hotmail.it>
2013-06-23 13:32:09 +0200

    Trasparent background

--- Jarvis/gen/com/example/text/R.java
+++ Jarvis/gen/com/example/text/R.java
@@ -43,15 +43,6 @@ public final class R {
     }
     public static final class style {
         /** 
-        Base application theme, dependent on API level. This theme is replaced
-        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
-    
-
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-        
-
         Base application theme for API 11+. This theme completely replaces
         AppBaseTheme from res/values/styles.xml on API 11+ devices.
     
@@ -63,10 +54,7 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
-        /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
-         */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppBaseTheme=0x7f060001;
+        public static final int Theme_Transparent=0x7f060000;
     }
 }
